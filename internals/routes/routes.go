31cea468c6d31644c1c35b3d9a56f670eb8af165
JDarwind <boscardingiacomo@gmail.com>
2025-12-31 20:29:24 +0100

    implemented new middleware in the default routing flow

--- internals/routes/routes.go
+++ internals/routes/routes.go
@@ -3,6 +3,8 @@ package routes
 import (
 	"net/http"
 	"fmt"
+	"github.com/JDarwind/go-skeleton-starter/pkg/network"
+	"github.com/JDarwind/go-skeleton-starter/internals/middlewares"
 )
 
 func helloWorld(w http.ResponseWriter,r *http.Request){
@@ -10,8 +12,15 @@ func helloWorld(w http.ResponseWriter,r *http.Request){
 }
 
 
-func NewRouter() *http.ServeMux {
+func NewRouter() http.Handler {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/hello-world/", helloWorld)
-	return mux;
+	
+	chain:= network.Chain(
+		mux,
+		middlewares.Logging,
+	)
+	
+
+	return chain;
 }
